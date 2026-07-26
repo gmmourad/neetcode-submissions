d72a11b9b962e55b330457cb9240989306141931
gmmourad <102994485+gmmourad@users.noreply.github.com>
2026-07-26 09:28:34 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0;
+        int j = s.length() -1;
+        while (i < j)
+        {
+            if (!isalnum(s[i]))
+            {
+                i++;
+                continue;
+            }
+            if (!isalnum(s[j]))
+            {
+                j--;
+                continue;
+            }
+            if (!((s[i] == s[j]) || (toupper(s[i]) == toupper(s[j]))))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
